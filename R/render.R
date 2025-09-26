7c4e66a2f5c60952d91b2283d62ab4e6af437069
David Keyes <david@rfortherestofus.com>
2025-09-26 09:49:03 -0700

    Update formatting

--- R/render.R
+++ R/render.R
@@ -7,99 +7,97 @@ library(fs)
 library(xfun)
 library(googledrive)
 
-
 # Import Data ------------------------------------------------------------
 source("R/import-data.R")
 
-
 # Load Data ---------------------------------------------------------------
 states_flags <- list.files(
-    path = "assets/flags",
-    pattern = "\\.svg$",
-    full.names = TRUE
+  path = "assets/flags",
+  pattern = "\\.svg$",
+  full.names = TRUE
 )
 states <- tools::file_path_sans_ext(basename(states_flags))
 states <- states[states != "puerto_rico"] # remove for now
 
 # Create State QMDs --------------------------------------------------------
 if (dir_exists("documents")) {
-    print("Deleting documents/ and reports/")
-    dir_delete("documents")
-    dir_delete("reports")
+  print("Deleting documents/ and reports/")
+  dir_delete("documents")
+  dir_delete("reports")
 }
 dir_create("documents/assets/flags")
 dir_create("reports")
 
 create_state_qmd <- function(state) {
-    file_copy(
-        path = "report.qmd",
-        new_path = str_glue("documents/{state}.qmd"),
-        overwrite = TRUE
-    )
+  file_copy(
+    path = "report.qmd",
+    new_path = str_glue("documents/{state}.qmd"),
+    overwrite = TRUE
+  )
 }
 walk(states, create_state_qmd)
 
 # Copy Assets -------------------------------------------------------------
 file_copy(
-    path = "assets/coins.svg",
-    new_path = "documents/assets/coins.svg"
+  path = "assets/coins.svg",
+  new_path = "documents/assets/coins.svg"
 )
 file_copy(
-    path = "assets/logo.png",
-    new_path = "documents/assets/logo.png"
+  path = "assets/logo.png",
+  new_path = "documents/assets/logo.png"
 )
 file_copy(
-    path = "assets/down-arrow.png",
-    new_path = "documents/assets/down-arrow.png"
+  path = "assets/down-arrow.png",
+  new_path = "documents/assets/down-arrow.png"
 )
 file_copy(
-    path = "assets/up-arrow.png",
-    new_path = "documents/assets/up-arrow.png"
+  path = "assets/up-arrow.png",
+  new_path = "documents/assets/up-arrow.png"
 )
 file_copy(
-    path = "typst-template.typ",
-    new_path = "documents/typst-template.typ"
+  path = "typst-template.typ",
+  new_path = "documents/typst-template.typ"
 )
 file_copy(
-    path = "typst-show.typ",
-    new_path = "documents/typst-show.typ"
+  path = "typst-show.typ",
+  new_path = "documents/typst-show.typ"
 )
 file_copy(
-    path = states_flags,
-    new_path = "documents/assets/flags"
+  path = states_flags,
+  new_path = "documents/assets/flags"
 )
 file_copy(
-    path = "R/charts.R",
-    new_path = "documents/charts.R"
+  path = "R/charts.R",
+  new_path = "documents/charts.R"
 )
 
 # Edit Parameters YAML ---------------------------------------------------
 change_parameters_yaml <- function(state) {
-    gsub_file(
-        file = str_glue("documents/{state}.qmd"),
-        "state:.*",
-        glue("state: ", gsub("_", " ", state))
-    )
+  gsub_file(
+    file = str_glue("documents/{state}.qmd"),
+    "state:.*",
+    glue("state: ", gsub("_", " ", state))
+  )
 }
 walk(states, change_parameters_yaml)
 
 # Edit Path Source ---------------------------------------------------------
 change_path_source <- function(state) {
-    gsub_file(
-        file = str_glue("documents/{state}.qmd"),
-        "R/charts.R",
-        "../R/charts.R" # Quarto uses relative paths
-    )
+  gsub_file(
+    file = str_glue("documents/{state}.qmd"),
+    "R/charts.R",
+    "../R/charts.R" # Quarto uses relative paths
+  )
 }
 walk(states, change_path_source)
 
 # Edit Path Data -----------------------------------------------------------
 change_path_data <- function(state) {
-    gsub_file(
-        file = str_glue("documents/{state}.qmd"),
-        pattern = "data-clean/",
-        replacement = "../data-clean/"
-    )
+  gsub_file(
+    file = str_glue("documents/{state}.qmd"),
+    pattern = "data-clean/",
+    replacement = "../data-clean/"
+  )
 }
 
 walk(states, change_path_data)
@@ -111,23 +109,23 @@ walk(str_glue("documents/{states}.qmd"), quarto_render)
 all_reports <- dir_ls(path = "documents", regexp = ".pdf")
 
 file_move(
-    path = all_reports,
-    new_path = "reports"
+  path = all_reports,
+  new_path = "reports"
 )
 
 # Upload Reports -----------------------------------------------------------
 if (str_detect(Sys.getenv("GOOGLE_DRIVE_EMAIL"), "rfortherestofus.com")) {
-    drive_auth(Sys.getenv("GOOGLE_DRIVE_EMAIL"))
+  drive_auth(Sys.getenv("GOOGLE_DRIVE_EMAIL"))
 
-    pdf_files <- list.files("reports", pattern = "\\.pdf$", full.names = TRUE)
+  pdf_files <- list.files("reports", pattern = "\\.pdf$", full.names = TRUE)
 
-    upload_report <- function(report_file) {
-        drive_upload(
-            media = report_file,
-            path = folder <- as_id("1fxoUQYyKK0ef4BRzb3Ab4w1x39s722su"),
-            overwrite = TRUE
-        )
-    }
+  upload_report <- function(report_file) {
+    drive_upload(
+      media = report_file,
+      path = folder <- as_id("1fxoUQYyKK0ef4BRzb3Ab4w1x39s722su"),
+      overwrite = TRUE
+    )
+  }
 
-    walk(pdf_files, upload_report)
+  walk(pdf_files, upload_report)
 }
